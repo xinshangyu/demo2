2c3b0b10114b62ed894570ff074166c9c974bb2c
admin@githost.com <jiuyankeji2013>
2019-08-29 17:36:51 +0800

    zh  阅读偏好

--- app/src/main/java/com/example/administrator/demo/activity/read/ReadActivity.java
+++ app/src/main/java/com/example/administrator/demo/activity/read/ReadActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
 import com.example.administrator.demo.R;
 import com.example.administrator.demo.adapter.Read1Adapter;
 import com.example.administrator.demo.adapter.Read2Adapter;
 import com.example.baselibrary.zh.base.BaseActivity;
+import com.example.baselibrary.zh.utils.flowlayout.FlowLayout;
+import com.example.baselibrary.zh.utils.flowlayout.FlowLayoutAdapter;
 
 import java.util.ArrayList;
 
@@ -20,10 +23,11 @@ public class ReadActivity extends BaseActivity {
 
     @BindView(R.id.RecyclerView)
     RecyclerView RecyclerView;
-    @BindView(R.id.RecyclerView2)
-    RecyclerView RecyclerView2;
+    @BindView(R.id.fl_search)
+    FlowLayout flowLayout;
 
     private ArrayList<String> mListData = new ArrayList<>();
+    private ArrayList<String> mListData2 = new ArrayList<>();
     private Read1Adapter adapter;
     private Read2Adapter adapter2;
 
@@ -47,16 +51,46 @@ public class ReadActivity extends BaseActivity {
     protected void initDate() {
 
         for (int i = 0; i < 5; i++) {
-            mListData.add("的的" + i);
+            mListData.add("的的f" + i);
+        }
+        for (int i = 0; i < 5; i++) {
+            if (i == 0)
+                mListData2.add("的的成第三" + i);
+            else if (i == 1) mListData2.add("的的成" + i);
+            else mListData2.add("的的成ac地方第三" + i);
         }
 
         RecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         adapter = new Read1Adapter(mContext, mListData);
         RecyclerView.setAdapter(adapter);
 
-        RecyclerView2.setLayoutManager(new LinearLayoutManager(mContext));
-        adapter2 = new Read2Adapter(mContext, mListData);
-        RecyclerView2.setAdapter(adapter2);
+//        RecyclerView2.setLayoutManager(new LinearLayoutManager(mContext));
+//        adapter2 = new Read2Adapter(mContext, mListData2);
+//        RecyclerView2.setAdapter(adapter2);
+
+        FlowLayoutAdapter<String> flowLayoutAdapter = new FlowLayoutAdapter<String>(mListData2) {
+            @Override
+            public void bindDataToView(FlowLayoutAdapter.ViewHolder holder, int position, String bean) {
+                holder.setText(R.id.tv_name, bean);
+            }
+
+            @Override
+            public void onItemClick(int position, String bean) {
+                Bundle bundle = new Bundle();
+
+//                bundle.putString(AppConfig.NAME,list.get(position).getName());
+//                bundle.putString(AppConfig.MID,list.get(position).getMid());
+//                bundle.putBoolean(AppConfig.ISAREA,true);
+//                ActivityUtils.startActivity(mContext,SearchAreaActivity.class,bundle);
+            }
+
+            @Override
+            public int getItemLayoutID(int position, String bean) {
+                return R.layout.item_search_cache;
+            }
+        };
+        flowLayout.setAdapter(flowLayoutAdapter);
+
 
     }
 }
--- app/src/main/java/com/example/administrator/demo/adapter/Read2Adapter.java
+++ app/src/main/java/com/example/administrator/demo/adapter/Read2Adapter.java
@@ -1,25 +1,61 @@
 package com.example.administrator.demo.adapter;
 
 import android.content.Context;
+import android.os.Bundle;
 
 import com.example.administrator.demo.R;
 import com.example.baselibrary.zh.adapter.CommonAdapter;
 import com.example.baselibrary.zh.adapter.base.ViewHolder;
+import com.example.baselibrary.zh.utils.ActivityUtils;
+import com.example.baselibrary.zh.utils.flowlayout.FlowLayout;
+import com.example.baselibrary.zh.utils.flowlayout.FlowLayoutAdapter;
 
 import java.util.ArrayList;
 
 public class Read2Adapter extends CommonAdapter<String> {
 
     private Context context;
+    private ArrayList<String> datas;
 
     public Read2Adapter(Context context, ArrayList<String> datas) {
-        super(context, R.layout.item_read1, datas);
+        super(context, R.layout.item_read2, datas);
         this.context = context;
+        this.datas = datas;
     }
 
     @Override
     protected void convert(ViewHolder holder, String messageListBean, int position) {
-        holder.setText(R.id.tv_name, messageListBean.toString());
+        FlowLayout flowLayout = holder.getView(R.id.fl_search);
+
+//        List<SearchBean.Regions.Desc> list = Imgs.getList();
+//        ArrayList<String> strings = new ArrayList<>();
+//        for (int i = 0; i < mDatas.size(); i++) {
+//            strings.add(messageListBean);
+//        }
+
+        FlowLayoutAdapter<String> flowLayoutAdapter = new FlowLayoutAdapter<String>(datas) {
+            @Override
+            public void bindDataToView(FlowLayoutAdapter.ViewHolder holder, int position, String bean) {
+                holder.setText(R.id.tv_name, bean);
+            }
+
+            @Override
+            public void onItemClick(int position, String bean) {
+                Bundle bundle = new Bundle();
+
+//                bundle.putString(AppConfig.NAME,list.get(position).getName());
+//                bundle.putString(AppConfig.MID,list.get(position).getMid());
+//                bundle.putBoolean(AppConfig.ISAREA,true);
+//                ActivityUtils.startActivity(mContext,SearchAreaActivity.class,bundle);
+            }
+
+            @Override
+            public int getItemLayoutID(int position, String bean) {
+                return R.layout.item_search_cache;
+            }
+        };
+        flowLayout.setAdapter(flowLayoutAdapter);
+
 
     }
 }
